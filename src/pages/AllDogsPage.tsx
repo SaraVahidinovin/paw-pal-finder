50d10ab9d3408540fc1fb7813b1b0cb6b7e54298
unknown <sa.vahidinovin@gmail.com>
2024-10-09 14:34:41 +0200

    Add useloaderdata and render dogs

--- src/pages/AllDogsPage.tsx
+++ src/pages/AllDogsPage.tsx
@@ -1,13 +1,29 @@
 import { useNavigate } from 'react-router-dom';
+import { useLoaderData} from 'react-router-dom';
+import { Dog } from '../types';
+import ProductCard from '../components/ProductCard';
 
 export default function AllDogsPage() {
+    const dogs = useLoaderData() as Dog[];
 
     // Hook to handle navigation
     const navigate = useNavigate(); 
 
     return (
         <>
+            <div>
+                <h1>Explore Dogs Ready For Adoption</h1>
+                <div className="card-container">
+                    {dogs.map((dog, index) => (
+                        <section
+                        key={index}
+                        style={{ cursor: "pointer" }}
+                    >
+                        <ProductCard dog={dog} />
+                    </section>
+                    ))}
+                </div>
+            </div>
         </>
-      
     )
 }
