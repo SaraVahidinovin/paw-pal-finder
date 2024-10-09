143182e1b4df61f743edc02fb237c14d2d766e1b
unknown <sa.vahidinovin@gmail.com>
2024-10-09 14:41:26 +0200

    Add dog interface

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface Dog {
+  name: string;
+  sex: string;
+  breed: string;
+  img: string;
+  present: boolean;
+  age: number;
+  chipNumber: string;
+  owner: {
+    name: string;
+    lastName: string;
+    phoneNumber: string;
+  };
+}
