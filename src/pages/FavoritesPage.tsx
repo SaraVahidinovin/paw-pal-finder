a6675f6e62a0c22d46d70d9674f179200b2962c0
unknown <sa.vahidinovin@gmail.com>
2024-10-09 14:47:05 +0200

    Render favorites

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,13 +1,44 @@
+import { useContext } from "react"
+import GlobalStateContext from "../context/GlobalStateContext"
+import ProductCard from "../components/ProductCard";
+import { Dog } from "../types";
 import { useNavigate } from 'react-router-dom';
 
 export default function FavoritesPage() {
 
-    // Hook to handle navigation
-    const navigate = useNavigate(); 
+    // Retrieve the favorites array from the global context
+    const { favorites } = useContext(GlobalStateContext);
+
+    // Hook to programmatically navigate between routes
+    const navigate = useNavigate();
+
+    // Function to handle clicking on a card
+    const handleCocktailClick = (dog: Dog) => {
+        // Navigate to the details page using the dog's chipNumber
+        navigate(`/dog/${favorites.chipNumber}`);
+    };
 
     return (
-        <>
-        </>
-      
+        <section className="main-content-container">
+            <h2>Your Favorite Cocktails</h2>
+            <section className="card-container">
+                {/* Check if there are any favorite dogs */}
+                {favorites.length > 0 ?
+                    // If there are favorites, map over the array and display each as a ProductCard component
+                    (favorites.map((dog, index) => (
+                        <section key={index} className="favorite-item" onClick={() => handleCocktailClick(dog)} style={{ cursor: 'pointer' }}>
+                            <ProductCard dog={dog} />
+                        </section>
+                    ))
+                    ) : (
+                        // If no favorites are found, display a message to the user
+                        <section className="not-found-container">
+                        <h3>No favorite dog found.
+                        </h3>
+                        </section>
+                    )
+                }
+            </section>
+        </section>
     )
 }
