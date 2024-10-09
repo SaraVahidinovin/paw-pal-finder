20dcb2b312bfc0b807cd063d9aa2dc118fca0a3f
unknown <sa.vahidinovin@gmail.com>
2024-10-09 14:45:41 +0200

    Add GlobalStateContextType

--- src/context/GlobalStateContext.tsx
+++ src/context/GlobalStateContext.tsx
@@ -1,14 +1,28 @@
 import { createContext, useState, ReactNode } from 'react';
+import { Dog } from '../types';
+
+interface GlobalStateContextType {
+  searchResults: Dog[];
+  setSearchResults: (dogs: Dog[]) => void;
+  favorites: Dog[];
+  setFavorites: (dogs: Dog[]) => void;
+}
+const defaultState: GlobalStateContextType = {
+  searchResults: [],
+  setSearchResults: () => {},
+  favorites: [],
+  setFavorites: () => {},
+};
 
 // Create the context
 
-const GlobalStateContext = createContext(null);
+const GlobalStateContext = createContext<GlobalStateContextType>(defaultState);
 
 
 // Create a provider 
 export const GlobalStateProvider = ({ children }: { children: ReactNode }) => {
-  const [searchResults, setSearchResults] = useState([]);
-  const [favorites, setFavorites] = useState([]);
+  const [searchResults, setSearchResults] = useState<Dog[]>([]);
+  const [favorites, setFavorites] = useState<Dog[]>([]);
 
   return (
     <GlobalStateContext.Provider value={{ searchResults, setSearchResults, favorites, setFavorites }}>
