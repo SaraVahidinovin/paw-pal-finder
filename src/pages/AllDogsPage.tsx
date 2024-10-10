fb1ec510844feb878f008a85713ce66555fd8239
unknown <sa.vahidinovin@gmail.com>
2024-10-10 21:23:21 +0200

    Add header

--- src/pages/AllDogsPage.tsx
+++ src/pages/AllDogsPage.tsx
@@ -2,15 +2,20 @@ import { useNavigate } from 'react-router-dom';
 import { useLoaderData} from 'react-router-dom';
 import { Dog } from '../types';
 import ProductCard from '../components/ProductCard';
+import Header from '../components/Header';
+import '../styles/allDogsPage.css'
 
 export default function AllDogsPage() {
     const dogs = useLoaderData() as Dog[];
 
     return (
-        <>
-            <div>
+        <section className="all-dogs-container">
+            <section className="header-container">
+            <Header />
+            </section>
+            <section className="all-dogs-content">
                 <h1>Explore Dogs Ready For Adoption</h1>
-                <div className="card-container">
+                <section className="card-container">
                     {dogs.map((dog, index) => (
                         <section
                         key={index}
@@ -18,8 +23,8 @@ export default function AllDogsPage() {
                         <ProductCard dog={dog} />
                     </section>
                     ))}
-                </div>
-            </div>
-        </>
+                </section>
+            </section>
+        </section>
     )
 }
