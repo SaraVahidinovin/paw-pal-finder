9f2f2dec97d8d2391cd92ca0fadc88bc5f9444d7
unknown <sa.vahidinovin@gmail.com>
2024-10-10 21:07:03 +0200

    Add logout icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { NavLink, useLocation } from "react-router-dom";
 import logo from '../assets/logo.png'
 import { useAuth } from "../context/GlobalAuthContext";
+import '../styles/header.css'
+import Logout from '../assets/Logout.svg'
 
 export default function Header() {
     const location = useLocation(); //  the current route location
@@ -11,53 +13,58 @@ export default function Header() {
 
     return (
         <nav className={headerClass}>
-        <NavLink to="/">
-            <img src={logo} className="logo" alt="Logo" />
-        </NavLink>
-        <NavLink
-            to="/alldogs"
-            className={({ isActive }) => isActive ? 'active-link' : ''}
-        >
-            All Dogs
-        </NavLink>
+            <div className="logo-container">
+                <NavLink to="/">
+                    <img src={logo} className="logo" alt="Logo" />
+                </NavLink>
+            </div>
+            <div className="menu-container">
+                <NavLink
+                    to="/alldogs"
+                    className={({ isActive }) => isActive ? 'active-link' : ''}
+                >
+                    All Dogs
+                </NavLink>
 
-        {!isLoggedIn ? (
-            // If not logged in, show login
-            <NavLink
-                to="/login"
-                className={({ isActive }) => isActive ? 'active-link' : ''}
-            >
-                Login
-            </NavLink>
-        ) : (
-            // If logged in, show My Dogs, Add a Dog, Favorites, and Logout
-            <>
-                <NavLink
-                    to="/mydogs"
-                    className={({ isActive }) => isActive ? 'active-link' : ''}
-                >
-                    My Dogs
-                </NavLink>
-                <NavLink
-                    to="/adddog"
-                    className={({ isActive }) => isActive ? 'active-link' : ''}
-                >
-                    Add a Dog
-                </NavLink>
-                <NavLink
-                    to="/favorites"
-                    className={({ isActive }) => isActive ? 'active-link' : ''}
-                >
-                    Favorites
-                </NavLink>
-                <button
-                    onClick={logout}
-                    className="logout-button"
-                >
-                    Logout
-                </button>
-            </>
-        )}
-    </nav>
+                {!isLoggedIn ? (
+                    // If not logged in, show login
+                    <NavLink
+                        to="/login"
+                        className={({ isActive }) => isActive ? 'active-link' : ''}
+                    >
+                        Login
+                    </NavLink>
+                ) : (
+                    // If logged in, show My Dogs, Add a Dog, Favorites, and Logout
+                    <>
+                        <NavLink
+                            to="/mydogs"
+                            className={({ isActive }) => isActive ? 'active-link' : ''}
+                        >
+                            My Dogs
+                        </NavLink>
+                        <NavLink
+                            to="/adddog"
+                            className={({ isActive }) => isActive ? 'active-link' : ''}
+                        >
+                            Add a Dog
+                        </NavLink>
+                        <NavLink
+                            to="/favorites"
+                            className={({ isActive }) => isActive ? 'active-link' : ''}
+                        >
+                            Favorites
+                        </NavLink>
+                        <button
+                            onClick={logout}
+                            className="logout-button"
+                            title="Logout"
+                        >
+                            <img src={Logout} alt="Logout"></img>
+                        </button>
+                    </>
+                )}
+            </div>
+        </nav>
     );
 }
