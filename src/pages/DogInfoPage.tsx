90b7eae6cf2ab068333d4bda2c0d6dc7cbae60a6
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-10-14 10:24:06 +0200

    Display dogs detailed info

--- src/pages/DogInfoPage.tsx
+++ src/pages/DogInfoPage.tsx
@@ -1,6 +1,7 @@
 import {Dog} from "../types"
 import { useLocation } from 'react-router-dom';
 import "../styles/dogInfoPage.css"
+import Header from '../components/Header';
 
 export default function DogInfoPage() {
     const location = useLocation();
@@ -8,25 +9,30 @@ export default function DogInfoPage() {
 
     return (
         <section className="dog-info-container">
-            <h2>Learn More About This Furry Friend</h2>
-            <div className="dog-info">
-                <img src={dog.img} alt={dog.name} className="dog-image" />
-                <div className="info-sections">
-                    <div className="dog-info-section">
-                        <h3>Dog Information</h3>
-                        <p><strong>Name: </strong>{dog.name}</p>
-                        <p><strong>Breed: </strong>{dog.breed}</p>
-                        <p><strong>Gender: </strong>{dog.sex}</p>
-                        <p><strong>Age: </strong>{dog.age} years old</p>
-                    </div>
-                    <div className="owner-info-section">
-                        <h3>Owner Information</h3>
-                        <p><strong>Owner's Name: </strong>{dog.owner.name}</p>
-                        <p><strong>Owner's Last Name: </strong>{dog.owner.lastName}</p>
-                        <p><strong>Phone Number: </strong>{dog.owner.phoneNumber}</p>
+            <section className="header-container">
+                <Header />
+            </section>
+            <section className="all-dogs-content">
+                <h2>Learn More About This Furry Friend</h2>
+                <div className="dog-info">
+                    <img src={dog.img} alt={dog.name} className="dog-image" />
+                    <div className="info-sections">
+                        <div className="dog-info-section">
+                            <h3>Dog Information</h3>
+                            <p><strong>Name: </strong>{dog.name}</p>
+                            <p><strong>Breed: </strong>{dog.breed}</p>
+                            <p><strong>Gender: </strong>{dog.sex}</p>
+                            <p><strong>Age: </strong>{dog.age} years old</p>
+                        </div>
+                        <div className="owner-info-section">
+                            <h3>Owner Information</h3>
+                            <p><strong>Owner's Name: </strong>{dog.owner.name}</p>
+                            <p><strong>Owner's Last Name: </strong>{dog.owner.lastName}</p>
+                            <p><strong>Phone Number: </strong>{dog.owner.phoneNumber}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </section>
     )
 }
