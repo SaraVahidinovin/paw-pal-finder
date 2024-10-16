3d22240250fc3489886d17007c6ef44558a7ab7a
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-10-16 23:49:08 +0200

    Added add-dog-container and imported dogForm component

--- src/pages/AddDogPage.tsx
+++ src/pages/AddDogPage.tsx
@@ -1,8 +1,20 @@
-export default function AddDogPage() {
+import Header from '../components/Header';
+import DogForm from '../components/CreateNewProduct';
+import '../styles/addEditDog.css'
+
+interface AddDogPageProps {
+    mode: "create" | "edit"
+}
+
+export default function AddDogPage({ mode } : AddDogPageProps) {
 
     return (
-        <>
-        </>
-      
+        <section className="add-dog-container">
+        <section className="header-container">
+            <Header />
+        </section>
+        <h2>Help a Dog Find Their Forever Family!</h2>
+        <DogForm mode={mode} />
+        </section>
     )
 }
