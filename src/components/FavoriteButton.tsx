092f8c4a175251591532f2b05fbc1b22b17c89cb
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-10-16 23:31:25 +0200

    Imported css file

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -4,6 +4,7 @@ import GlobalStateContext from '../context/GlobalStateContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+import '../styles/favoriteButton.css'
 
 interface FavoriteButtonProps {
     dog: Dog;
