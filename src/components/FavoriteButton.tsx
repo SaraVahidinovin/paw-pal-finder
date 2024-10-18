8befddb60101060f3cb9d6f58654b310514a2b08
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-10-18 22:24:43 +0200

    Updated filter based on id

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -14,7 +14,7 @@ export default function FavoriteButton({ dog }: FavoriteButtonProps) {
     const { favorites, setFavorites } = useContext(GlobalStateContext);
 
     // Check if the current dog is already in the list of favorites
-    const isFavorite = favorites.some(favorite => favorite.chipNumber === dog.chipNumber);
+    const isFavorite = favorites.some(favorite => favorite.id === dog.id);
 
     // Add the current dog to the favorites list
     function addFavorite() {
@@ -23,7 +23,7 @@ export default function FavoriteButton({ dog }: FavoriteButtonProps) {
 
     // Remove the current dog from the favorites list
     function removeFavorite() {
-        setFavorites(favorites.filter(favorite => favorite.chipNumber !== dog.chipNumber));
+        setFavorites(favorites.filter(favorite => favorite.id !== dog.id));
     }
 
     // Toggle between adding and removing the dog from favorites
@@ -39,7 +39,7 @@ export default function FavoriteButton({ dog }: FavoriteButtonProps) {
     return (
         <button className="favorite-icon" onClick={handleFavoriteClick}
             style={{ color: isFavorite ? 'red' : 'white' }}>
-            <FontAwesomeIcon icon={isFavorite ? faHeartSolid : faHeartRegular} />
+            <FontAwesomeIcon icon={isFavorite ? faHeartSolid : faHeartRegular} title={isFavorite ? 'Remove from favorite': 'Add to favorite'} />
         </button>
     );
 }
